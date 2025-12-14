9b39bf4ae4c05782ac3ca204a30a80ed7a24989f
Hunter Rosenblume <hrosenblume@users.noreply.github.com>
2025-12-14 15:50:01 -0500

    Add comment to Prisma singleton

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 
+// Singleton Prisma client to prevent multiple instances in development
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma = globalForPrisma.prisma || new PrismaClient()
